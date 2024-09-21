d7e5919b99937da313020fb49fa33ab5634c0f7b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-20 22:17:48 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("seu nome");
+Console.WriteLine(
+    Console.ReadLine()!
+    .Replace("a" ,"4")
+    .Replace("e","3")
+    .Replace("i", "1")
+    .Replace("I", "1")
+    .Replace("o", "0")
+    .Replace("O", "0")
+    .Replace("A", "4")
+    .Replace("E", "3")
+    
+    
+);
